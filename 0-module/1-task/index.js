1b226b36915607bb9449a4986647ba1c800ca944
fkluck <116810810+fkluck@users.noreply.github.com>
2023-02-09 00:30:59 +0300

    вывел функцию для двух задач

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,3 +1,12 @@
-function sum(m, n) {
-  // ваш код...
+{function sum(m, n) {
+  return m + n;
+}
+let result = sum(1, 1);
+alert( result ); // 3
+}
+{function sum(m, n) {
+  return m + n;
+}
+let result = sum(2, 5);
+alert( result ); // 7
 }
