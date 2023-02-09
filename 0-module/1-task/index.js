014c7e333d344e4ee66570595f11f4ce77136da2
fkluck <116810810+fkluck@users.noreply.github.com>
2023-02-09 19:08:33 +0300

    ?

--- 0-module/1-task/index.js
+++ 0-module/1-task/index.js
@@ -1,12 +1,5 @@
-{function sum(m, n) {
+function sum(m, n) {
   return m + n;
 }
-let result = sum(1, 1);
-alert( result ); // 2
-}
-{function sum(m, n) {
-  return m + n;
-}
-let result = sum(2, 5);
-alert( result ); // 7
-}
+let result = sum(1,1) === 2;
+let result2 = sum(2,5) === 7;
