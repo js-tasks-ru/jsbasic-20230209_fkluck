bc122577ca9868f1915fe4e4288dda9bfd08077b
fkluck <116810810+fkluck@users.noreply.github.com>
2023-02-14 23:51:22 +0300

    сделал 5 функций вместо 1

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,35 @@
 function factorial(n) {
-  // ваш код...
+  let result = 1;
+  while(n){
+      result *= n--;
+  }
+  return result;
 }
+factorial(0);
+
+function factorial(n) {
+  let result = 1;
+  while(n){
+      result *= n--;
+  }
+  return result;
+}
+factorial(1);
+
+function factorial(n) {
+  let result = 1;
+  while(n){
+      result *= n--;
+  }
+  return result;
+}
+factorial(3);
+
+function factorial(n) {
+  let result = 1;
+  while(n){
+      result *= n--;
+  }
+  return result;
+}
+factorial(5);
