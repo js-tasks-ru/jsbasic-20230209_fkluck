3458ab9b8b5c5b643126a09f6f34960cfba72f58
fkluck <116810810+fkluck@users.noreply.github.com>
2023-02-15 20:53:31 +0300

    исправил

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,35 +1,7 @@
 function factorial(n) {
-  let result = 1;
-  while(n){
-      result *= n--;
+    let result = 1;
+    while(n){
+    result *= n--;
+    }
+    return result;
   }
-  return result;
-}
-factorial(0);
-
-function factorial(n) {
-  let result = 1;
-  while(n){
-      result *= n--;
-  }
-  return result;
-}
-factorial(1);
-
-function factorial(n) {
-  let result = 1;
-  while(n){
-      result *= n--;
-  }
-  return result;
-}
-factorial(3);
-
-function factorial(n) {
-  let result = 1;
-  while(n){
-      result *= n--;
-  }
-  return result;
-}
-factorial(5);
