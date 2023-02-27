cb412e7d32016dbc2413525e341028be1d60f90d
fkluck <116810810+fkluck@users.noreply.github.com>
2023-02-27 23:00:23 +0300

    3 из 5

--- 3-module/1-task/index.js
+++ 3-module/1-task/index.js
@@ -1,3 +1,13 @@
+let vasya = { name: 'Вася', age: 25 };
+let petya = { name: 'Петя', age: 30 };
+let masha = { name: 'Маша', age: 28 };
+
+let users = [ vasya, petya, masha ];
+let names = namify(users);
+
 function namify(users) {
-  // ваш код...
-}
+  let names = users.map((user) => user.name);
+return names;
+  }
+
+
